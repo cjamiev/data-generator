97854ecf875434ce4d149f18f59cb96f30f3a8d0
Chris Varughese <cjamiev@gmail.com>
2025-01-01 08:50:14 -0600

    feat: updating styles of navigation

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,22 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { RoutesUrl, RoutesTitle } from '../types/routes';
+
+const Urls = Object.keys(RoutesUrl);
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className="h-16 w-full">
       <nav className="ml-2 pb-2 pt-2">
         <ul className="flex flex-row gap-2">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/data-generator">Data Generator</Link>
-          </li>
-          <li>
-            <Link to="/password-generator">Password Generator</Link>
-          </li>
-          <li>
-            <Link to="/encode">Encode</Link>
-          </li>
+          {Urls.map(link => {
+            const linkUrl = RoutesUrl[link as keyof typeof RoutesUrl];
+            const linkTitle = RoutesTitle[link as keyof typeof RoutesTitle];
+            const isActive = location.pathname === linkUrl;
+
+            return (<li key={link}>
+              <a className={`cursor-pointer p-4 ${isActive ? 'font-bold' : ''}`} onClick={() => navigate(linkUrl)}>{linkTitle}</a>
+            </li>)
+          })}
         </ul>
       </nav>
     </div>
---
+++ src/types/routes.ts
@@ -0,0 +1,13 @@
+export enum RoutesUrl {
+  HOME = '/',
+  DATA_GENERATOR = '/data-generator',
+  PASSWORD_GENERATOR = '/password-generator',
+  ENCODE = '/encode'
+}
+
+export enum RoutesTitle {
+  HOME = 'Home',
+  DATA_GENERATOR = 'Data Generator',
+  PASSWORD_GENERATOR = 'Password Generator',
+  ENCODE = 'Encode'
+} 
