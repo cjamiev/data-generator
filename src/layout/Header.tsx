4b096ba68d4561485daa4060679c62ed923c0ddd
Chris Varughese <cjamiev@gmail.com>
2025-01-01 15:32:31 -0600

    feat: adding more options to paragraph page

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -11,14 +11,18 @@ const Header = () => {
     <div className="h-16 w-full">
       <nav className="ml-2 pb-2 pt-2">
         <ul className="flex flex-row gap-2">
-          {Urls.map(link => {
+          {Urls.map((link) => {
             const linkUrl = RoutesUrl[link as keyof typeof RoutesUrl];
             const linkTitle = RoutesTitle[link as keyof typeof RoutesTitle];
             const isActive = location.pathname === linkUrl;
 
-            return (<li key={link}>
-              <a className={`cursor-pointer p-4 ${isActive ? 'font-bold' : ''}`} onClick={() => navigate(linkUrl)}>{linkTitle}</a>
-            </li>)
+            return (
+              <li key={link}>
+                <a className={`cursor-pointer p-4 ${isActive ? 'font-bold' : ''}`} onClick={() => navigate(linkUrl)}>
+                  {linkTitle}
+                </a>
+              </li>
+            );
           })}
         </ul>
       </nav>
