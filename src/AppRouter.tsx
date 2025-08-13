a33f802ab2bde968b7a4bb5287652a142091a4f3
Chris Varughese <cjamiev@gmail.com>
2025-08-12 21:02:56 -0500

    feat: removing encode page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,7 +5,6 @@ import { HomePage } from './pages/HomePage';
 import { DataGeneratorPage } from './pages/DataGeneratorPage';
 import { PasswordGeneratorPage } from './pages/PasswordGeneratorPage';
 import { SettingsPage } from './pages/SettingsPage';
-import { EncodePage } from './pages/EncodePage';
 
 export default function AppRouter() {
   return (
@@ -15,7 +14,6 @@ export default function AppRouter() {
           <Route index element={<HomePage />} />
           <Route path="data-generator" element={<DataGeneratorPage />} />
           <Route path="password-generator" element={<PasswordGeneratorPage />} />
-          <Route path="encode" element={<EncodePage />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
--- src/types/routes.ts
+++ src/types/routes.ts
@@ -2,7 +2,6 @@ export enum RoutesUrl {
   HOME = '/',
   DATA_GENERATOR = '/data-generator',
   PASSWORD_GENERATOR = '/password-generator',
-  ENCODE = '/encode',
   SETTINGS = '/settings',
 }
 
@@ -10,6 +9,5 @@ export enum RoutesTitle {
   HOME = 'Home',
   DATA_GENERATOR = 'Data',
   PASSWORD_GENERATOR = 'Password',
-  ENCODE = 'Encode',
   SETTINGS = 'Settings'
 } 
